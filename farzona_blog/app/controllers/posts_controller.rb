74309c36ec7bf6db8d8e671f3db157f8debeccf0
suttonau <sutton_mjbc@icloud.com>
2018-10-27 13:19:39 -0400

    Post Controller

---
+++ farzona_blog/app/controllers/posts_controller.rb
@@ -0,0 +1,56 @@
+class PostsController < ApplicationController
+  before action :find_post, :only [:edit, :update, :show, :delete]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new
+    if post.save(post_params)
+      flash[:notice] = "Successfully created post!"
+    else
+      flash[:alert] = "Error creating post :("
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update_attributes(post_params)
+      flash[:notice] = "Successfully updated post!"
+      redirect_to post_path(@posts)
+    else
+      flash[:alert] = "Error updating post! :("
+      render :edit
+    end
+  end
+
+  def show
+  end
+
+  def destroy
+    if @post.destroy
+      flash[:notice] = "Successfully deleted post!"
+      redirect_to posts_path
+    else
+      flash[:alert] = "Error deleting post!"
+    end 
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+end
